7db8f8866a4a9b7bc73391d3214c957802c96dd4
SonghoYun <itlockit@gmail.com>
2020-07-13 21:16:33 +0900

    Updates

---
+++ precache-manifest.61222fcf1d93ab589fa1cf823156c9b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae09448ece93dde3eab12396f348d853",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "a4706030ac54af5da9ee",
+    "url": "/movie_app_2019/static/css/main.412ca1fa.chunk.css"
+  },
+  {
+    "revision": "b52157e597c6acbc702d",
+    "url": "/movie_app_2019/static/js/2.fe2d4bf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2019/static/js/2.fe2d4bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4706030ac54af5da9ee",
+    "url": "/movie_app_2019/static/js/main.f7981950.chunk.js"
+  },
+  {
+    "revision": "10a8e46fe0d1ddf64ca9",
+    "url": "/movie_app_2019/static/js/runtime-main.15105636.js"
+  }
+]);
